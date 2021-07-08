14c7f92f052279eeb6c7aef2f30707f12a86f1e9
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2021-07-08 16:41:07 +0300

    done with lesson 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,56 @@
 const headerCityButton = document.querySelector('.header__city-button');
+const subheaderCart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
+headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
+
+headerCityButton.addEventListener('click', () => {
+   const city = prompt("Какой у Вачс город ?", "Харьков");
+
+   headerCityButton.textContent = city;
+   localStorage.setItem('lomoda-location', city);
+});
+//scroll blocked 
+const disableScroll = () => {
+   const widthScroll = window.innerWidth - document.body.offsetWidth;
+   document.body.dbScrollY = window.scrollY;
+
+   document.body.style.cssText = `
+      position: fixed;
+      width: 100%;
+      height: 100vh;
+      overflow: hidden;
+      padding-right: ${widthScroll}px;
+      top: ${-document.body.dbScrollY}px;
+      left: 0;
+   `
+};
+
+const enableScroll = () => {
+   document.body.style.cssText = '';
+   window.scroll({
+      top: document.body.dbScrollY
+   })
+}
+
+//dialog 
+const cartModalOpen = () => {
+   cartOverlay.classList.add('cart-overlay-open');
+   disableScroll();
+};
+
+const cartModalClose = () => {
+   cartOverlay.classList.remove('cart-overlay-open');
+   enableScroll();
+};
+
+subheaderCart.addEventListener('click',  cartModalOpen);
+
+cartOverlay.addEventListener('click', event => {
+   const target = event.target;
+
+   if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+      cartModalClose();
+   }
+});
 
